821419da861c989432910da767dae0c0f9fd9299
suminhohu <1500160640@qq.com>
2018-03-11 23:26:12 +0800

    实现商品详情首图和title部分
    不过添加商品组件引入动画不能正确执行

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="goods">
-    <div class="menu-wrapper" ref="menuWrapper">
-      <ul>
-        <li v-for="(item, index) in goods" class="menu-item" :class="{'current': currentIndex === index}"
-            @click="selectMenu(index, $event)" ref="menuList">
-          <span class="text border-1px">
-            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
-            {{ item.name }}
-          </span>
-        </li>
-      </ul>
+  <div>
+    <div class="goods">
+      <div class="menu-wrapper" ref="menuWrapper">
+        <ul>
+          <li v-for="(item, index) in goods" class="menu-item" :class="{'current': currentIndex === index}"
+              @click="selectMenu(index, $event)" ref="menuList">
+            <span class="text border-1px">
+              <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+              {{ item.name }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+          <li v-for="item in goods" class="food-list" ref="foodList">
+            <h1 class="title">{{ item.name }}</h1>
+            <ul>
+              <li @click="selectFood(food, $event)" v-for="food in item.foods" class="food-item border-1px">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{ food.name }}</h2>
+                  <p class="desc">{{ food.description }}</p>
+                  <div class="extra">
+                    <span>月售{{ food.sellCount }}份</span>
+                    <span>好评率{{ food.rating }}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{ food.price }}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>`
+      </div>
+      <shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
     </div>
-    <div class="foods-wrapper" ref="foodsWrapper">
-      <ul>
-        <li v-for="item in goods" class="food-list" ref="foodList">
-          <h1 class="title">{{ item.name }}</h1>
-          <ul>
-            <li v-for="food in item.foods" class="food-item border-1px">
-              <div class="icon">
-                <img width="57" height="57" :src="food.icon">
-              </div>
-              <div class="content">
-                <h2 class="name">{{ food.name }}</h2>
-                <p class="desc">{{ food.description }}</p>
-                <div class="extra">
-                  <span>月售{{ food.sellCount }}份</span>
-                  <span>好评率{{ food.rating }}%</span>
-                </div>
-                <div class="price">
-                  <span class="now">￥{{ food.price }}</span>
-                  <span class="old" v-show="food.oldPrice">￥{{ food.oldPrice }}</span>
-                </div>
-                <div class="cartcontrol-wrapper">
-                  <cartcontrol @add="addFood" :food="food"></cartcontrol>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </li>
-      </ul>`
-    </div>
-    <shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <food @add="addFood" :food="selectedFood" ref="food"></food>
   </div>
 </template>
 
@@ -48,6 +51,7 @@
   import BScroll from "better-scroll"
   import shopcart from "../../components/shopcart/shopcart.vue"
   import cartcontrol from "../../components/cartcontrol/cartcontrol.vue"
+  import food from "../../components/food/food.vue"
 
   const ERR_OK = 0;
   const debug = process.env.NODE_ENV !== 'production';
@@ -62,7 +66,8 @@
       return {
         goods: [],
         listHeight: [],
-        scrollY: 0
+        scrollY: 0,
+        selectedFood: {}
       }
     },
     computed: {
@@ -111,6 +116,11 @@
 
         this.foodsScoll.scrollToElement(el, 300)
       },
+      selectFood(food, event){
+        if (!event._constructed) return
+        this.selectedFood = food
+        this.$refs.food.show()
+      },
       addFood(target) {
         this._drop(target)
       },
@@ -153,6 +163,7 @@
     components: {
       shopcart,
       cartcontrol,
+      food
     }
   }
 </script>
@@ -254,7 +265,7 @@
             .now
               font-size 14px
               color rgb(240, 20, 20)
-              margin-right 18px
+              margin-right 8px
             .old
               font-size 10px
               color rgb(147, 153, 159)
